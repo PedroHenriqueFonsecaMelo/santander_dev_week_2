c5c59ead8619b12d4bbf1c1cc02fdc4c7748a2c8
PedroHenriqueFonsecaMelo <83472390+PedroHenriqueFonsecaMelo@users.noreply.github.com>
2024-07-16 13:41:58 -0300

    first commit

---
+++ src/test/java/me/dio/santander_dev_week_2/SantanderDevWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_week_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
